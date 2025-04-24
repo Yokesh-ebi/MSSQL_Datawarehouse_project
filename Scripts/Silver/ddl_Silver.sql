1a33fa88fe041b0e9ef60ba62e3cc9ee8be99607
Yokesh-ebi <yokeshebi100@gmail.com>
2025-04-24 11:06:35 +0530

    Update ddl_Silver.sql

--- Scripts/Silver/ddl_Silver.sql
+++ Scripts/Silver/ddl_Silver.sql
@@ -1,1 +1,93 @@
+If object_Id('silver.crm_cust_info','U') Is not null
+	Drop Table silver.crm_cust_info
+Go
+
+Create Table silver.crm_cust_info(
+	cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE,
+	dwh_create_date     Datetime2 default getdate()
+)
+Go
+
+If object_Id('silver.crm_prd_info','U') Is not null
+	Drop Table silver.crm_prd_info
+Go
+
+create table silver.crm_prd_info(
+
+	prd_id       INT,
+	cat_id		 NVARCHAR(50),
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
+	dwh_create_date     Datetime2 default getdate()
+)
+Go
+
+If object_Id('silver.crm_sales_details','U') Is not null
+	Drop Table silver.crm_sales_details
+Go
+
+Create Table silver.crm_sales_details(
+	sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT,
+	dwh_create_date     Datetime2 default getdate()
+)
+Go
+
+If object_Id('silver.erp_loc_a101','U') Is not null
+	Drop Table silver.erp_loc_a101
+Go
+
+Create Table silver.erp_loc_a101(
+
+	cid					NVARCHAR(50),
+    cntry				NVARCHAR(50),
+	dwh_create_date     Datetime2 default getdate()
+
+)
+Go
+
+If object_Id('silver.erp_cust_az12','U') Is not null
+	Drop Table silver.erp_cust_az12
+Go
+
+Create Table silver.erp_cust_az12(
+
+	cid					NVARCHAR(50),
+    bdate				DATE,
+    gen					NVARCHAR(50),
+	dwh_create_date     Datetime2 default getdate()
+
+)
+Go
+
+If object_Id('silver.erp_px_cat_g1v2','U') Is not null
+	Drop Table silver.erp_px_cat_g1v2
+Go
+
+Create Table silver.erp_px_cat_g1v2(
+
+	id					NVARCHAR(50),
+    cat					NVARCHAR(50),
+    subcat				NVARCHAR(50),
+    maintenance			NVARCHAR(50),
+	dwh_create_date     Datetime2 default getdate()
+)
+Go
 
